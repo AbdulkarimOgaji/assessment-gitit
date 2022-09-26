d9da0c4003021dafc504c3148dc9ec6d0920aa08
AbdulkarimOgaji <abdulkarimogaji002@gmail.com>
2022-09-26 09:18:27 +0100

    style: add shadow to navbar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,9 +1,25 @@
 import { NavLink } from "react-router-dom";
 import classes from "./navbar.module.css";
+import { useEffect, useState } from "react";
 
 const NavBar = () => {
+  const [shadow, setShadow] = useState({});
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY < 10) {
+        setShadow({ boxShadow: "none" });
+      } else {
+        setShadow({});
+      }
+    };
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
   return (
-    <header className={classes.navbar}>
+    <header className={classes.navbar} style={shadow}>
       <h1>GitIt</h1>
       <ul>
         <li>
