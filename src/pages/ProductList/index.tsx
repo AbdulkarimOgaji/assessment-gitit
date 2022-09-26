987faffa60f76e9d9b584330aec972c327eb8877
AbdulkarimOgaji <abdulkarimogaji002@gmail.com>
2022-09-26 09:56:27 +0100

    style: product list page

--- src/pages/ProductList/index.tsx
+++ src/pages/ProductList/index.tsx
@@ -1,6 +1,31 @@
+import ProductCard from "../../components/ProductCard";
 import classes from "./product-list.module.css";
 const ProductList = () => {
-  return <h1 className={classes.container}>Product List Page</h1>;
+  return (
+    <section className={classes.container}>
+      <div className={classes.featured}>
+        <img src="/featured.jfif" />
+        <div>
+          <h1>Featured Product</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque
+            ducimus iure tenetur quas voluptate placeat reprehenderit commodi
+            repudiandae! Aliquam cupiditate vitae, id, facere, repellat
+            voluptatibus reiciendis ipsam soluta voluptas blanditiis quisquam.
+            Odio temporibus qui praesentium omnis tempore fugit sed non?
+          </p>
+          <button>Create a New Product</button>
+        </div>
+      </div>
+      <section className={classes.productsContainer}>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </section>
+    </section>
+  );
 };
 
 export default ProductList;
