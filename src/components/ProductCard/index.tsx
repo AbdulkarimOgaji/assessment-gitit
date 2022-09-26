ff0122f00c28565e0ff1999ca231c29ae910f880
AbdulkarimOgaji <abdulkarimogaji002@gmail.com>
2022-09-26 09:56:59 +0100

    style: do product card component

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+import classes from "./product.module.css";
+
 const ProductCard = () => {
-  return <></>;
+  return (
+    <div className={classes.card}>
+      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
+      <p>Created At something</p>
+      <Link to="/products/2">See more</Link>
+    </div>
+  );
 };
 
 export default ProductCard;
